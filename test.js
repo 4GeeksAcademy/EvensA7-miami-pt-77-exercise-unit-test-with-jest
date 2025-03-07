8aaf7f2674de207c9fcdece84138c45bc8bade71
Evens Antoine <evens7antoine@gmail.com>
2025-03-07 02:03:48 +0000

    Done

--- test.js
+++ test.js
@@ -11,29 +11,22 @@ test('adds 14 + 9 to equal 23', () => {
 });
 
 test("One euro should be 1.07 dollars", function() {
-    // Import the function from app.js
+   
     const { fromEuroToDollar } = require('./app.js');
-
-    // Use the function like its supposed to be used
-    const dollars = fromEuroToDollar(3.5);
-
-    // If 1 euro is 1.07 dollars, then 3.5 euros should be (3.5 * 1.07)
-    const expected = 3.5 * 1.07; 
     
-    // This is the comparison for the unit test
      expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
 })
 
-test("One euro should be 1.07 dollars", function() {
-    // Import the function from app.js
-    const { fromDollarToYen } = require('./app.js');
-
-    // Use the function like its supposed to be used
-    const yen = fromDollarToYen(3.5);
-
-    // If 1 euro is 1.07 dollars, then 3.5 euros should be (3.5 * 1.07)
-    const expected = 3.5 * 1.07; 
+test("One dollar should be 149.03 yen", function() {
     
-    // This is the comparison for the unit test
-     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
+    const { fromDollarToYen } = require('./app.js'); 
+    
+     expect(fromDollarToYen(10)).toBe(1493.3);
+})
+
+test("One yen should be 0.0072 pound)", function() {
+   
+    const { fromYenToPound } = require('./app.js')
+    
+    expect(fromYenToPound(1000)).toBe(7.2);
 })
